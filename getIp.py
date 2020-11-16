203ed7187232b79083a806315014f4582de91860
1000in1 <1000in1@sina.com>
2020-11-16 15:29:32 +0000

    update

--- getIp.py
+++ getIp.py
@@ -2,12 +2,13 @@ import urllib.request
 import subprocess
 import socket
 import re
+import time
  
 child=subprocess.Popen("ifconfig", shell=True, stdout = subprocess.PIPE)
-out=child.communicate();#保存 ipconfig 中的所有信息
- 
+out=child.communicate()
 ipv6_pattern='(([a-f0-9]{1,4}:){7}[a-f0-9]{1,4})'
 m=re.findall(ipv6_pattern,str(out));
 address=m[1][0]
-print(m[0][0])
-print(m[1][0])
+t=time.time()
+print(t,m[0][0])
+print(t,m[1][0])
