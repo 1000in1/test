c8cfc138602b3416dd1b3390fd3a4ed06a242174
1000in1 <1000in1@>
2020-11-17 02:20:22 +0800

    update

--- getIp.py
+++ getIp.py
@@ -4,11 +4,20 @@ import socket
 import re
 import time
  
-child=subprocess.Popen("ifconfig", shell=True, stdout = subprocess.PIPE)
-out=child.communicate()
-ipv6_pattern='(([a-f0-9]{1,4}:){7}[a-f0-9]{1,4})'
-m=re.findall(ipv6_pattern,str(out));
-address=m[1][0]
-t=time.strftime('%Y/%m/%d %H:%M:%S')
-print(t,m[0][0])
-print(t,m[1][0])
+#child=subprocess.Popen("ifconfig", shell=True, stdout = subprocess.PIPE)
+#out=child.communicate()
+#ipv6_pattern='(([a-f0-9]{1,4}:){7}[a-f0-9]{1,4})'
+#m=re.findall(ipv6_pattern,str(out));
+#address=m[1][0]
+#t=time.strftime('%Y/%m/%d %H:%M:%S')
+#print(t,m[0][0])
+#print(t,m[1][0])
+
+
+import urllib.request
+import json
+response = urllib.request.urlopen('http://[2001:470:1:18::115]/ip/?callback=c')
+html = response.read()
+s = str(html[2:-2],'utf-8')
+j = json.loads(s)
+print(t,j['ip'])
