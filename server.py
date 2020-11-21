471123d2963429f3025857a04e85e1577baec643
1000in1 <1000in1@>
2020-11-21 22:49:48 +0800

    新增文件上传post

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ class ServerHTTP(http.server.BaseHTTPRequestHandler):
         #print('path:',path)
         #拆分url(也可根据拆分的url获取Get提交才数据),可以将不同的path和参数加载不同的html页面，或调用不同的方法返回不同的数据，来实现简单的网站或接口
         query = urllib.parse.urlparse(path)
-        query.hostname
+        #query.hostname
         #print('urlparse path',query)
         #print('query',query.path)
   
@@ -36,21 +36,20 @@ class ServerHTTP(http.server.BaseHTTPRequestHandler):
         
     def do_POST(self):
         path = self.path
-        print(path)
-        #获取post提交的数据
-        datas = self.rfile.read(int(self.headers['content-length']))
-        datas = urllib.parse.unquote(datas.decode("utf-8", 'ignore'))
-        print("datas:",datas)
-        self.send_response(200)
-        self.send_header("Content-type","text/html")
-        self.send_header("test","This is test!")
-        self.end_headers()
-        buf = '''<!DOCTYPE HTML>
-        <html>
-            <head><title>Post page</title></head>
-            <body>Post Data:%s  <br />Path:%s</body>
-        </html>'''%(datas,self.path)
-        self.wfile.write(buf.encode(encoding="utf-8"))
+        query = urllib.parse.urlparse(path)
+        
+        
+        if query.path in self.postHandler.keys():
+          self.postHandler[query.path](self)
+        else:
+          self.send_error(404,'not found!') 
+
+
+    
+    
+    
+    
+        
 
     def init(self,p,m):
       def decorator(fc):
@@ -86,7 +85,56 @@ def func1(self):
   self.wfile.write(buf.encode(encoding="utf-8"))
 
 
+@ServerHTTP.init(ServerHTTP,"/",['POST'])
+def func2(self):
+    path = self.path
+    print(path)
+    print(self.headers)
+    name = self.headers['n']
+    print(name)
+    cmd = self.headers['cmd']
+    print(cmd)
 
+    query = urllib.parse.urlparse(path)
+    print('query',query)
+        
+    #获取post提交的数据
+    fo = open("../share/%s"%name, "wb")
+    le = int(self.headers['content-length'])
+
+
+    self.send_response(200)
+    self.send_header("Content-type","text/html")
+    self.send_header("test","This is test!")
+    self.end_headers()
+
+    buf = '''<!DOCTYPE HTML>
+    <html>
+        <head><title>Post page</title></head>
+        <body>file:%s  <br />size:%d'''%(name,le)
+    self.wfile.write(buf.encode(encoding="utf-8"))
+
+    l = 0
+    while l<le:
+      i=1024
+      if l+1024>le:
+        i = le-l
+
+      datas = self.rfile.read(i)
+      l = l+len(datas)
+      line = fo.write( datas )
+      buf = '%d/%d<br>'%(l,le)
+      print(buf)
+      self.wfile.write(buf.encode(encoding="utf-8"))
+
+    fo.close()
+
+    
+    buf = '''</body>
+    </html>'''
+    self.wfile.write(buf.encode(encoding="utf-8"))
+
+    
 
 
 
